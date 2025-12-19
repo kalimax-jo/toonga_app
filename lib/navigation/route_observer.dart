5e26bfd77c094f5d4feb643547530a5203fd13b2
kalimax-jo <josueasik@gmail.com>
2025-12-19 11:37:10 +0200

    Add route observer for navigator

---
+++ lib/navigation/route_observer.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+/// Shared route observer used to detect when routes are covered or uncovered.
+final RouteObserver<ModalRoute<dynamic>> appRouteObserver =
+    RouteObserver<ModalRoute<dynamic>>();
